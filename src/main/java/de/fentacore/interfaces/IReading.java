fbd171c602c0bc52f1e1797c7a965e4d3a2516bc
Julius <onhaxyt@gmail.com>
2024-09-28 01:11:39 +0200

    Add Interfaces

---
+++ src/main/java/de/fentacore/interfaces/IReading.java
@@ -0,0 +1,41 @@
+package de.fentacore.interfaces;
+
+import java.time.LocalDate;
+
+public interface IReading extends IId {
+
+   enum KindOfMeter {
+      HEIZUNG, STROM, UNBEKANNT, WASSER;
+   }
+
+   String getComment();
+
+   ICustomer getCustomer();
+
+   LocalDate getDateOfReading();
+
+   KindOfMeter getKindOfMeter();
+
+   Double getMeterCount();
+
+   String getMeterId();
+
+   Boolean getSubstitute();
+
+   String printDateOfReading();
+
+   void setComment(String comment);
+
+   void setCustomer(ICustomer customer);
+
+   void setDateOfReading(LocalDate dateOfReading);
+
+   void setKindOfMeter(KindOfMeter kindOfMeter);
+
+   void setMeterCount(Double meterCount);
+
+   void setMeterId(String meterId);
+
+   void setSubstitute(Boolean substitute);
+
+}
